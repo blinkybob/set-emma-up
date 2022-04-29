164da9ae1700669c965a42ea37744036634d16cc
blinkybob <michael.binder01@telekom.de>
2022-04-29 13:19:27 +0200

    changed tools

--- files/.bash_aliases
+++ files/.bash_aliases
@@ -11,17 +11,12 @@ alias nikto='perl /home/michi/tools/nikto/program/nikto.pl'
 
 #testssl
 alias testssl='bash /home/michi/tools/testssl.sh/testssl.sh'
-# testssl Docker
-alias docker-testssl='sudo docker run -ti drwetter/testssl.sh'
 
 #######################
 #Public IPs
 alias myip6='curl -6 ifconfig.co'
 alias myip4='curl -4 ifconfig.co'
 #alias myip4='dig +short myip.opendns.com @resolver1.opendns.com'
-
-#mount command better readable
-alias mount='mount | column -t'
 
 #wget resume by default
 alias wget='wget -c'
@@ -30,20 +25,31 @@ alias wget='wget -c'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
 
+#docker aliases
+alias docker-testssl='sudo docker run -ti drwetter/testssl.sh'
 
 # alias for changing in routerlab directory
 alias cpelab='cd /home/michi/sec/1_Project/CPE/cpelab/'
 alias cpelab-git='cd /home/michi/sec/1_Project/CPE/cpelab/Labor/gitlab.cpelab.de/'
 
+#waitforssh till ping succesfull
+alias waitforssh="/home/michi/tools/waitforssh/wait.sh"
+
+# Git multi pull all repos in directory subdir
+alias multipull-git='find . -mindepth 1 -maxdepth 3 -type d -print -exec git -C {} pull \;'
+
 #spice connection routerlab vm 333 debian
-alias cpe-spice-333='bash /home/michi/sec/1_Project/CPE/cpelab/Labor/gitlab.cpelab.de/snippets/spice-v01.sh -u michael@pve -p <PW> 333 kermit'
+alias cpe-spice-333='bash /home/michi/sec/1_Project/CPE/cpelab/Labor/gitlab.cpelab.de/snippets/spice-v01.sh -u michael@pve -p Golf30802 333 kermit'
 
 #spice connection routerlab vm 334 kali
-alias cpe-spice-334='bash /home/michi/sec/1_Project/CPE/cpelab/Labor/gitlab.cpelab.de/snippets/spice-v01.sh -u michael@pve -p <PW> 334 kermit'
+alias cpe-spice-334='bash /home/michi/sec/1_Project/CPE/cpelab/Labor/gitlab.cpelab.de/snippets/spice-v01.sh -u michael@pve -p Golf30802 334 kermit'
 
 ### vpn metric changes ###
 alias metwifi='echo "setting metric for wifi vpn shit..." && sudo ifmetric wlp3s0 45'
 alias metlan='echo "setting metric for lan vpn shit..." && sudo ifmetric enp0s31f6 45'
 
-### change batcat to bat
+###
+alias wget='wget -c'
+
+###
 alias bat='batcat'
--- setup.sh
+++ setup.sh
@@ -4,14 +4,16 @@
 # local installation / new setup of laptop
 # 18.01.2022
 # Installations where user input is needed are at the end of the script, to keep it running more or less without user interaction
+# 28.04.2022 - lots of changes, 
 
-# to-do: farben reinbringen für den output, ganzes apt glump wegloggen
-# ubuntu os codename etc. als variable für etcher input oder anderes zeug!!!
+# Usage hint: choose your distro, codename, etc. in the vars section
 
 # Vars
 DISTROBASE=ubuntu
-CODENAME=impish
-VERSION=21.10
+#CODENAME=impish
+CODENAME=jammy
+#VERSION=21.10
+VERSION=22.04
 #WORKDIR=/home/"$(whoami)"/
 mkdir -p /home/"$(whoami)"/tools
 
@@ -20,7 +22,7 @@ RED="\e[31m"
 GREEN="\e[32m"
 EC="\e[0m"
 
-# update system
+# update OS
 echo -e "${GREEN}Updating OS...${EC}"
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -36,50 +38,108 @@ echo -e "${GREEN}Installing basic tools...${EC}"
 sudo apt-get install -y snapd flameshot kazam kvmtool virt-viewer virt-manager spice-client-gtk spice-html5 spice-webdavd \
     spice-vdagent remmina tmux zsh git openvpn network-manager-openvpn network-manager-openvpn-gnome \
     wireguard wireguard-dkms wireguard-tools firefox thunderbird filezilla pinta \
-    shotwell p7zip-full p7zip-rar bat unzip ncdu flatpak nextcloud-desktop
+    shotwell p7zip-full p7zip-rar bat unzip ncdu flatpak nextcloud-desktop 
 
 # Installation docker
 sudo apt-get install -y docker.io docker-compose
 
+# Docker Conatiners
+# testssl
+
 # Download and installation Postman
 echo -e "${GREEN}Downloading and installing Postman...${EC}"
-curl https://dl.pstmn.io/download/latest/linux64 -o postman-latest.tar.gz
+curl https://dl.pstmn.io/download/latest/linux64 -o /home/"$(whoami)"/tools/postman-latest.tar.gz
 tar -xf postman-latest.tar.gz -C /home/"$(whoami)"/tools/
 
 
 # install webex Teams
-# install MS Teams
 
-# security tools
-sudo apt-get install -y nikto hping3 wireshark tcpdump tshark nmap
+if [ -d "/opt/Webex/" ]
+then
+    echo "${RED}Webex already installed.${EC}"
+else
+    echo -e "${GREEN}Downloading and installing Webex Teams...${EC}"
+    wget 'https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb' -O /home/"$(whoami)"/tools/set-emma-up/files/webex.deb
+    sudo dpkg -i /home/"$(whoami)"/tools/set-emma-up/files/webex.deb
+fi
+
+# install MS Teams
+#echo -e "${GREEN}Downloading and installing MS Teams...${EC}"
+
+
+# sec tools
+echo -e "${GREEN}Sec Tools...${EC}"
+sudo apt-get install -y nikto hping3 wireshark tcpdump tshark nmap python3-scapy sslscan minicom sqlitebrowser tableplus
 sudo usermod -aG wireshark "$(whoami)"
 
 # gnome extensions
 sudo apt-get install -y gnome-shell-extensions
-echo "not possible to install the extensions via CLI script"
+echo "${RED}not possible to install the extensions via CLI script${EC}"
 
 # balena etcher
-echo -e "${GREEN}Adding Etcher repo...${EC}"
-curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | distro=$DISTROBASE version=$VERSION codename=$CODENAME sudo -E bash
+# check balena installed
 
-echo -e "${GREEN}Installing Etcher...${EC}"
-sudo apt-get update && sudo apt-get install -y balena-etcher-electron
+if [ -f "/opt/balenaEtcher/balena-etcher-electron" ]
+then
+    echo "${RED}Etcher already installed.${EC}"
+else
+    echo -e "${GREEN}Adding Etcher repo...${EC}"
+    curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | distro=$DISTROBASE version=$VERSION codename=$CODENAME sudo -E bash
 
-# Ulauncher
-echo -e "${GREEN}Adding uLauncher repo and installing...${EC}"
-sudo add-apt-repository -y ppa:agornostal/ulauncher
-sudo apt-get update && sudo apt-get -y install ulauncher
+    echo -e "${GREEN}Installing Etcher...${EC}"
+    sudo apt-get update && sudo apt-get install -y balena-etcher-electron
+fi
+
+# WOE USB for creating windows boot stick in linux
+#https://omgubuntu.co.uk/2017/06/create-bootable-windows-10-usb-ubuntu
+
+if [ -f "/usr/bin/woeusb" ]
+then
+    echo "${RED}Woeusb already installed.${EC}"
+else
+    echo -e "${GREEN}Adding WoeUSB repo and installing...${EC}"
+    sudo add-apt-repository ppa:tomtomtom/woeusb -y
+    sudo apt update && sudo apt install woeusb-frontend-wxgtk -y
+fi
+
+# Ulauncher check and installation
+if [ -f "/usr/bin/ulauncher" ]
+then
+    echo "${RED}Ulauncher already installed.${EC}"
+else
+    echo -e "${GREEN}Adding uLauncher repo and installing...${EC}"
+    sudo add-apt-repository -y ppa:agornostal/ulauncher
+    sudo apt-get update && sudo apt-get -y install ulauncher
+fi
+
+#threema desktop client
+if [ -f "/usr/bin/threema" ]
+then
+    echo "${RED}Threema already installed.${EC}"
+else
+    echo -e "${GREEN}Downloading and installing Threema client...${EC}"
+    wget https://releases.threema.ch/web-electron/v1/release/Threema-Latest.deb -O Threema-latest.deb
+    dpkg -i Threema-latest.deb 
+fi
+
 
 # snaps
 
-echo "Installing Snap Spotify ... "
-sudo snap install spotify
-echo "Installing snap VSCodium..."
+#echo -e "${GREEN}Installing Snap Spotify...${EC}"
+#sudo snap install spotify
+echo -e "${GREEN}Installing snap VSCodium...${EC}"
 sudo snap install codium --classic
-echo "Installing snap telegram..."
+echo -e "${GREEN}Installing snap telegram...${EC}"
 sudo snap install telegram-desktop
 
 # VSCodium Extensions ?
+
+# flatpaks
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+# Gnome Extension Manager
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+# Spotify
+flatpak install flathub com.spotify.Client -y
 
 # Install Sublime text editor
 echo -e "${GREEN}Adding Sublime Text repo and installing...${EC}"
@@ -89,7 +149,7 @@ sudo apt-get install -y sublime-text
 
 # Sublime Extensions
 
-# install virtualbox
+# Virtualbox
 
 echo -e "${GREEN}Installing VirtualBox...${EC}"
 sudo sh -c 'echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian eoan contrib" >> /etc/apt/sources.list.d/virtualbox.list'
@@ -113,11 +173,15 @@ wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2_11_1_uni
 sudo apt-get install -y tmux-plugin-manager
 
 #dotfiles
+
+# tmux
 echo -e "${RED}HINT: Copying tmux dotfiles and plugin directory...${EC}"
 cp files/.tmux.conf /home/"$(whoami)"/.tmux.conf
-cp -r files/.tmux /home/"$(whomai)"/.tmux
-
+cp -r files/.tmux /home/"$(whoami)"/.tmux
+# bash aliases
 cp files/.bash_aliases /home/"$(whoami)"/
 
+# ulauncher
+cp -r files/ulauncher /home/"$(whoami)"/.config/
 
 echo -e "${RED}Installation finished...Please reboot the system!!!${EC}\n"
